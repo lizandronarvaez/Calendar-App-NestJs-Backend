8e883c84f6e99d62d0012d60976d1a6501a4ef86
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-11-29 09:15:26 +0100

    modelo autenticacion/registro base de datos

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+@Schema()
+export class Auth extends Document {
+  @Prop({
+    unique: true,
+    type: String,
+    required: true,
+  })
+  fullname: string;
+
+  @Prop({
+    unique: true,
+    type: String,
+    required: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  password: string;
+}
+
+export const AuthSchema = SchemaFactory.createForClass(Auth);
+
+// Metodo para encriptar la password
+AuthSchema.pre('save', async function () {
+  try {
+    if (this.isNew || this.isModified('password')) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
