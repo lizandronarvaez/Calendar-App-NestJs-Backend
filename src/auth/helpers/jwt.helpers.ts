2bd6c95a6ed34e58b8657025ba490e5093292fd3
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-11-29 09:16:47 +0100

    funciones para validar datos y generar token

---
+++ src/auth/helpers/jwt.helpers.ts
@@ -0,0 +1,31 @@
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../interfaces/JwtPayload.interface';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+@Injectable()
+export class JwtHelper {
+  constructor(private jwtservice: JwtService) {}
+
+  generateTokenJwt(payload: JwtPayload) {
+    const token = this.jwtservice.sign(payload);
+    return token;
+  }
+
+  isValidToken(authorization: string) {
+    if (!authorization) {
+      throw new UnauthorizedException('No existe token');
+    }
+    const token = authorization.split(' ')[1];
+    try {
+      const tokenIsValid = this.jwtservice.verify(token, {
+        secret: process.env.SECRET_KEY,
+      });
+
+      //! TODO: Crear logica para reestablecer la password una vez verificamos que el token es valido
+      return tokenIsValid;
+    } catch (error) {
+      console.log(error);
+      throw new UnauthorizedException('El token no es válido');
+    }
+  }
+}
