6f3e77147d3c7f86f79970115663adbe3de85aec
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-11-29 09:21:40 +0100

    controladores CRUD de eventos

---
+++ src/events/events.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { EventsService } from './events.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('events')
+export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  //! TODO: Todas las rutas deberan esta protegidas
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  createEvent(@Body() createEventDto: CreateEventDto) {
+    return this.eventsService.create(createEventDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  findAllEvents() {
+    return this.eventsService.findAll();
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get(':id')
+  findOneEvent(@Param('id') id: string) {
+    return this.eventsService.findOne(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put(':id')
+  updateEvent(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
+    return this.eventsService.update(id, updateEventDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  removeEvent(@Param('id') id: string) {
+    return this.eventsService.remove(id);
+  }
+}
