43d7bfec7fcc06b21a2e3af844b92a899ea8e95e
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-12-05 17:37:57 +0100

    Aplicando archivo barril para importaciones

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -5,47 +5,44 @@ import {
   Body,
   Param,
   Delete,
-  UseGuards,
   Patch,
 } from '@nestjs/common';
+import { Auth_Auto } from 'src/auth/decorators/auth.decorators';
+import { Role } from 'src/auth/rol/rol.enum';
+import { CreateEventDto, UpdateEventDto } from './dto';
 import { EventsService } from './events.service';
-import { CreateEventDto } from './dto/create-event.dto';
-import { UpdateEventDto } from './dto/update-event.dto';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
-  //! TODO: Todas las rutas deberan esta protegidas
-  @UseGuards(AuthGuard('jwt'))
+  @Auth_Auto(Role.User)
   @Post()
   createEvent(@Body() createEventDto: CreateEventDto) {
     return this.eventsService.create(createEventDto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @Auth_Auto(Role.User)
   @Get()
   findAllEvents() {
     return this.eventsService.findAll();
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @Auth_Auto(Role.User)
   @Get(':id')
   findOneEvent(@Param('id') id: string) {
     return this.eventsService.findOne(id);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @Auth_Auto(Role.User)
   @Patch(':id')
   updateEvent(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
     return this.eventsService.update(id, updateEventDto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @Auth_Auto(Role.User)
   @Delete(':id')
   removeEvent(@Param('id') id: string) {
-    console.log(id);
     return this.eventsService.remove(id);
   }
 }
